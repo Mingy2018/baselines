92a6481d46e4be0741fb5a492862e3be3f061dcf
Mingy2018 <39735862+Mingy2018@users.noreply.github.com>
2019-06-09 14:04:28 +0200

    Update models.py

--- baselines/common/models.py
+++ baselines/common/models.py
@@ -35,9 +35,12 @@ def nature_cnn(unscaled_images, **conv_kwargs):
     CNN from Nature paper.
     """
     scaled_images = tf.cast(unscaled_images, tf.float32) / 255.
-    activ = tf.nn.relu
+    activ = tf.nn.relu # calculate the Rectitied Linear Unit
+    
     h = activ(conv(scaled_images, 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2),
                    **conv_kwargs))
+    # np.sqrt() returns the squareroot of every element in the array
+    # init_scale ??
     h2 = activ(conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2), **conv_kwargs))
     h3 = activ(conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
     h3 = conv_to_fc(h3)
